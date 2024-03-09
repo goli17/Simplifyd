14e6496440563d3ea38dede8c9fd4ffb6c41d212
Saurabh <saurabhdewli@gmail.com>
2024-03-09 15:44:21 +0530

    font

--- src/Components/About CEO/Aboutdata.js
+++ src/Components/About CEO/Aboutdata.js
@@ -6,7 +6,7 @@ export const AboutusData = [
     title: "Simbit mallick",
     position: "Business & Tech",
     description:
-      "We take full responsibility for the delivery process, minimizing risk for the client and consistently delivering high-quality market-ready software products on time and within budget based on clients needs and requirements We take full responsibility for the delivery process, minimizing risk for the client and consistently ",
+      "We take full responsibility for the delivery process, minimizing risk for the client and consistently delivering high-quality market-ready software products on time and within budget based on clients needs and requirements. We take full responsibility for the delivery process, minimizing risk for the client and consistently ",
     image: ceo_img,
   },
   {
